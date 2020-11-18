c1ee7accf037ef7cc9e35fcfac27770e16cd9de8
Seamus Quinn <seamus@winnie.com>
2020-11-18 09:55:49 -0700

    Remove expiration_status

--- api/lunch.js
+++ api/lunch.js
@@ -11,7 +11,7 @@ module.exports = (req, res) => {
     "profile": {
       "status_text": "Lunch",
       "status_emoji": ":pizza:",
-      "status_expiration": 1605655403,
+      // "status_expiration": 1605655403,
     }
   }
   fetch('https://slack.com/api/users.profile.set', {
